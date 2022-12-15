71d1ec2d7bf6dfcd7db89e090c174a519fb5b3a7
Dario Camonita <danterolle@catania.linux.it>
2022-12-15 14:42:37 +0100

    Added http server to serve packages.json

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	filter "package-filter/src"
 )
+
+func getPackages(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "packages.json")
+}
 
 func main() {
 	p := new(filter.Package)
@@ -12,10 +18,21 @@ func main() {
 	if _, err := os.Stat("./packages"); err == nil {
 		fmt.Println("Parsing packages...")
 		p.Parser()
+		fmt.Println("Done. Check packages.json")
 	} else {
 		fmt.Println("Downloading packages...")
-		filter.GetPackages("./packages", "https://download.parrot.sh/parrot/dists/parrot/main/binary-amd64/Packages")
+		err := filter.GetPackages("./packages", "https://download.parrot.sh/parrot/dists/parrot/main/binary-amd64/Packages")
+		if err != nil {
+			log.Fatal(err)
+		}
 		fmt.Println("Done! Filtering...")
 		defer p.Parser()
 	}
+
+	http.HandleFunc("/packages/", getPackages)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
