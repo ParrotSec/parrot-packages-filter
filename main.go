6589d2704acddf747ef3334858a8347b11ea74be
Dario Camonita <danterolle@catania.linux.it>
2022-12-16 11:52:15 +0100

    Added all architectures

--- main.go
+++ main.go
@@ -14,9 +14,19 @@ func getARM64Packages(w http.ResponseWriter, req *http.Request) {
 	http.ServeFile(w, req, "arm64-packages.json")
 }
 
+func getARMHFPackages(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "armhf-packages.json")
+}
+
+func geti386Packages(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "i386-packages.json")
+}
+
 func handleFunctions() {
 	http.HandleFunc("/packages/main/amd64/", getAMD64Packages)
 	http.HandleFunc("/packages/main/arm64/", getARM64Packages)
+	http.HandleFunc("/packages/main/armhf/", getARMHFPackages)
+	http.HandleFunc("/packages/main/i386/", geti386Packages)
 }
 
 func main() {
@@ -26,7 +36,13 @@ func main() {
 	f := new(filter.Package)
 
 	log.Println("Downloading packages...")
-	arch := []string{"amd64"} // for more architectures, add "arm64", "armhf", "i386"
+	arch := []string{
+		"amd64",
+		"arm64",
+		"armhf",
+		"i386",
+	}
+
 	for i := range arch {
 		errDownload := filter.DownloadPackages(
 			"./"+arch[i]+"-packages",
@@ -39,7 +55,7 @@ func main() {
 
 	log.Println("Filtering...")
 	f.Parser()
-	log.Printf("[!] Starting HTTP server at port: %d\n", port)
+	log.Printf("[!] Starting HTTP server at port: %s\n", port)
 
 	handleFunctions()
 	errHttp := http.ListenAndServe(":"+port, nil)
--- src/filter.go
+++ src/filter.go
@@ -29,51 +29,63 @@ type PackageSet struct {
 }
 
 func (p *Package) Parser() {
-	file, _ := os.Open("./amd64-packages")
-
-	scanner := bufio.NewScanner(file)
-	buf := make([]byte, 0, 64*1024)
-	scanner.Buffer(buf, 1024*1024)
-
-	lineNumber := 0
-
-	var P PackageSet
-	P.Packages = make(map[string]Package)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		if strings.HasPrefix(line, prefixName) {
-			name := strings.TrimPrefix(line, prefixName)
-			p.Name = name
-		} else if strings.HasPrefix(line, prefixDesc) {
-			desc := strings.TrimPrefix(line, prefixDesc)
-			p.Description = desc
-		} else if strings.HasPrefix(line, prefixVersion) {
-			version := strings.TrimPrefix(line, prefixVersion)
-			p.Version = version
-		} else if strings.HasPrefix(line, prefixMaintainer) {
-			maintainer := strings.TrimPrefix(line, prefixMaintainer)
-			p.Maintainer = maintainer
-		} else if strings.HasPrefix(line, prefixArch) {
-			arch := strings.TrimPrefix(line, prefixArch)
-			p.Architecture = arch
-		}
-
-		P.Packages[p.Name] = Package{
-			Name:         p.Name,
-			Description:  p.Description,
-			Version:      p.Version,
-			Maintainer:   p.Maintainer,
-			Architecture: p.Architecture,
-		}
-
-		lineNumber++
+	version := map[string]string{
+		"amd64": "./amd64-packages",
+		"arm64": "./arm64-packages",
+		"armhf": "./armhf-packages",
+		"i386":  "./i386-packages",
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("Error on line %v: %v", lineNumber, err)
+	for i := range version {
+		file, _ := os.Open(version[i])
+
+		scanner := bufio.NewScanner(file)
+		buf := make([]byte, 0, 64*1024)
+		scanner.Buffer(buf, 1024*1024)
+
+		lineNumber := 0
+
+		var P PackageSet
+		P.Packages = make(map[string]Package)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if strings.HasPrefix(line, prefixName) {
+				name := strings.TrimPrefix(line, prefixName)
+				p.Name = name
+			} else if strings.HasPrefix(line, prefixDesc) {
+				desc := strings.TrimPrefix(line, prefixDesc)
+				p.Description = desc
+			} else if strings.HasPrefix(line, prefixVersion) {
+				version := strings.TrimPrefix(line, prefixVersion)
+				p.Version = version
+			} else if strings.HasPrefix(line, prefixMaintainer) {
+				maintainer := strings.TrimPrefix(line, prefixMaintainer)
+				p.Maintainer = maintainer
+			} else if strings.HasPrefix(line, prefixArch) {
+				arch := strings.TrimPrefix(line, prefixArch)
+				p.Architecture = arch
+			}
+
+			P.Packages[p.Name] = Package{
+				Name:         p.Name,
+				Description:  p.Description,
+				Version:      p.Version,
+				Maintainer:   p.Maintainer,
+				Architecture: p.Architecture,
+			}
+
+			lineNumber++
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatalf("Error on line %v: %v", lineNumber, err)
+		}
+		data, _ := json.MarshalIndent(P, "", "\t")
+		errWriteFile := os.WriteFile(version[i]+".json", data, 0644)
+		if errWriteFile != nil {
+			log.Fatalf("Can't create and write to %s", errWriteFile)
+		}
 	}
-	data, _ := json.MarshalIndent(P, "", "\t")
-	os.WriteFile("amd64-packages.json", data, 0644)
 }
