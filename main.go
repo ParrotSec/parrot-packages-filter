557ec404f280bb6e9c2a24e97699924af4f9d796
Dario Camonita <danterolle@catania.linux.it>
2023-01-12 12:56:36 +0100

    Cleaned main.go

--- main.go
+++ main.go
@@ -5,31 +5,14 @@
 package main
 
 import (
-	"errors"
 	"log"
-	"os"
 	filter "package-filter/internal"
 )
-
-var branch = [3]string{
-	"contrib",
-	"main",
-	"non-free",
-}
-
-var arch = [4]string{
-	"amd64",
-	"arm64",
-	"armhf",
-	"i386",
-}
 
 // Here the three phases of the program are carried out:
 // 1. Download the Packages for all the architectures.
 // 2. Filter and return them as JSON files (~70 mb).
 func main() {
-	const url = "https://download.parrot.sh/parrot/dists/parrot"
-
 	f := new(filter.Package)
 
 	// Create temporary dir called "packages"
@@ -39,36 +22,7 @@ func main() {
 	log.Println("[info] Downloading packages...")
 
 	// Use the DownloadPackages function to download Packages for each branch and architecture
-	for b := range branch {
-
-		errBranchDir := os.Mkdir("packages/"+branch[b], os.ModePerm)
-		if errBranchDir != nil {
-			log.Fatal(errBranchDir)
-		}
-
-		for a := range arch {
-			// Check and if not exists create a new JSON folder where to store each new JSON file for each branch and architecture
-			jsonPath := "json/packages/" + branch[b] + "/" + arch[a] + "/"
-
-			if _, errStatJson := os.Stat(jsonPath); errors.Is(errStatJson, os.ErrNotExist) {
-
-				errJsonFolder := os.MkdirAll(jsonPath, os.ModePerm)
-				if errJsonFolder != nil {
-					log.Fatal(errJsonFolder)
-				}
-
-			}
-
-			// Start downloading packages for all branches and architectures available
-			errDownload := filter.DownloadPackages(
-				"packages/"+branch[b]+"/"+arch[a],
-				url+"/"+branch[b]+"/binary-"+arch[a]+"/Packages")
-			if errDownload != nil {
-				log.Fatal(errDownload)
-			}
-
-		}
-	}
+	filter.GetJsonPackages()
 
 	// The filter phase begins.
 	log.Println("[info] Filtering...")
